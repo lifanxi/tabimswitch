9fc6579a863b3c7a36c00dd67dcdf6adb8058d5e
ftofficer.zhangc <ftofficer.zhangc@237747d1-5336-0410-8d3f-2982d197fc3e>
2007-07-30 16:41:41 +0000

    Changed the tab-im related features to component, to support multiple window.

--- trunk/src/tabimswitch/copy-comp.cmd
+++ trunk/src/tabimswitch/copy-comp.cmd
@@ -1,3 +1,2 @@
 copy ".\%1\tabimswitch.dll" "..\..\components\tabimswitch.dll"
 copy ".\tabimswitch.xpt" "..\..\components\tabimswitch.xpt"
-..\gecko-sdk\bin\regxpcom.exe -x "%cd%\..\..\components" -c "%_firefox_profile_path%\compreg.dat" -d "%_firefox_profile_path%\xpti.dat"
---
+++ trunk/src/tabimswitch/reg-comp-config.cmd
@@ -0,0 +1,7 @@
+@echo off
+rem Set this value to your development FireFox profile directory.
+rem Typically, it is under
+rem C:\Documents and Settings\<Your User Name>\Application Data\Mozilla\Firefox\Profiles\<something>.default
+rem If you are using multiple profile, use the corresponding one.
+
+set _firefox_profile_path=E:\zhangc\.WinCfg\Application Data\Mozilla\Firefox\Profiles\3lb9nvsc.dev
