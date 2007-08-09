cd8f102c81515222e21b936d7c985b3ad3b8ccd7
ftofficer.zhangc <ftofficer.zhangc@237747d1-5336-0410-8d3f-2982d197fc3e>
2007-08-09 14:19:24 +0000

    Removed the debug branch

--- tags/debug-1.0.0.8/src/tabimswitch/reg-comp.cmd
+++
@@ -1,5 +0,0 @@
-if exist reg-comp-config.cmd call reg-comp-config.cmd
-
-..\gecko-sdk\bin\regxpcom.exe -x "%cd%\..\..\components" -c "%_firefox_profile_path%\compreg.dat" -d "%_firefox_profile_path%\xpti.dat"
-del "%_firefox_profile_path%\compreg.dat"
-del "%_firefox_profile_path%\xpti.dat"
