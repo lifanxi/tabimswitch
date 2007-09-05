84a4aa7447367d9870ed95e68c378292f822ffa4
ftofficer.zhangc <ftofficer.zhangc@237747d1-5336-0410-8d3f-2982d197fc3e>
2007-09-05 16:50:13 +0000

    the release tag 1.1.0.37

---
+++ tags/release-1.1.0.37/incBuild.pl
@@ -0,0 +1,15 @@
+#! /usr/bin/perl -w
+#
+use strict;
+
+open FILE, "install.rdf" or die "no install.rdf: $!\n";
+my @file = <FILE>;
+close FILE;
+
+my $fileContent = join "", @file;
+
+$fileContent =~ s[<em:version>(\d+\.\d+\.\d+)\.(\d+)</em:version>]["<em:version>$1.".($2+1)."</em:version>"]ei;
+
+open OUTPUT, ">install.rdf" or die "cannot open install.rdf: $!\n";
+print OUTPUT $fileContent;
+close OUTPUT;
