30643c8f56f13ff100b4d57c782ade97eede505f
ftofficer.zhangc <ftofficer.zhangc@237747d1-5336-0410-8d3f-2982d197fc3e>
2008-06-12 17:07:27 +0000

    Create release tag release-1.1.0.44

---
+++ tags/release-1.1.0.44/tools/release-xpi.cmd
@@ -0,0 +1,26 @@
+@echo off
+setlocal
+
+if "%1"=="" (
+  call "%~dp0get-cur-version.cmd"
+  if errorlevel 1 goto usage
+) else (
+  set _ver=%1
+)
+
+googlecode_upload -s "[Release] TabIMSwitch %_ver%" -p tabimswitch -u ftofficer.zhangc -l "Featured,Firefox,Extension,Type-Installer,OpSys-Windows" output\tabimswitch-%_ver%.xpi
+if errorlevel 1 goto error
+
+goto finish
+
+:usage
+echo %~n0.cmd [version]
+echo.
+exit /b 1
+
+:error
+echo Error execute command.
+exit /b 2
+
+:finish
+exit /b 0
---
+++ tags/release-1.1.0.44/update_url.pl
@@ -0,0 +1,34 @@
+#! /usr/bin/perl -w
+#
+use strict;
+
+my $usage="update_url.pl [remove|restore]";
+my $command = shift or die $usage;
+
+if ( $command eq 'remove' ) {
+  open FILE, "install.rdf" or die "no install.rdf: $!\n";
+  my @file = <FILE>;
+  close FILE;
+
+  my $fileContent = join "", @file;
+
+  open BAK, ">install.rdf.bak" or die "cannot backup install.rdf:$!\n";
+  print BAK $fileContent;
+  close BAK;
+
+  $fileContent =~ s[<em:updateURL>.+</em:updateURL>][]i;
+  $fileContent =~ s[<em:updateKey>.+</em:updateKey>][]i;
+
+  open OUTPUT, ">install.rdf" or die "cannot open install.rdf: $!\n";
+  print OUTPUT $fileContent;
+  close OUTPUT;
+}
+elsif ( $command eq 'restore' ) {
+  unlink("install.rdf");
+  rename("install.rdf.bak", "install.rdf") or die ("Cannot restore install.rdf.bak to install.rdf: $!\n");
+}
+else {
+  die $usage;
+}
+
+exit 1;
