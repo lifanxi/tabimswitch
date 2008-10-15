029d6e4b300c86237b7c02f7388f7969f7ccc67c
ftofficer.zhangc <ftofficer.zhangc@237747d1-5336-0410-8d3f-2982d197fc3e>
2008-10-15 11:24:49 +0000

    Remove wrong tag for release 1.1.0.45

--- tags/release-1.1.0.45/defaults/preferences/tabimswitch.js
+++
@@ -1,6 +0,0 @@
-// See http://kb.mozillazine.org/Localize_extension_descriptions
-pref("extensions.TabImSwitch@ftofficer.description", "chrome://tabimswitch/locale/tabimswitch.properties");
-pref("extensions.tabimswitch.debug.level", 2);
-pref("extensions.tabimswitch.debug.prefix", "TabIMSwitch");
-pref("extensions.tabimswitch.debug.log_to_file", false);
-// pref("extensions.tabimswitch.env.multilingual", false);
--- tags/release-1.1.0.45/trunk/src/tabimswitch/copy-comp.cmd
+++
@@ -1,2 +0,0 @@
-copy ".\%1\tabimswitch.dll" "..\..\components\tabimswitch.dll"
-copy ".\tabimswitch.xpt" "..\..\components\tabimswitch.xpt"
